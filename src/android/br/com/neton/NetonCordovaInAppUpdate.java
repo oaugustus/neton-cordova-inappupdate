2eb19a67b8ce37bbcc99e297dfc3e5c2b5f9f230
Otávio Fernandes <oaugustus@gmail.com>
2019-08-23 19:43:35 -0300

    Fix

--- src/android/br/com/neton/NetonCordovaInAppUpdate.java
+++ src/android/br/com/neton/NetonCordovaInAppUpdate.java
@@ -19,6 +19,8 @@ import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 import com.google.android.play.core.install.model.UpdateAvailability;
 import com.google.android.play.core.tasks.Task;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
+import android.app.Activity;
+import android.content.IntentSender;
 
 
 import android.util.Log;
@@ -26,56 +28,70 @@ import android.util.Log;
 import java.util.Date;
 
 public class NetonCordovaInAppUpdate extends CordovaPlugin {
-  private static final String TAG = "NetonCordovaInAppUpdate";
-  private static final int MY_REQUEST_CODE = 1337;
+    private static final String TAG = "NetonCordovaInAppUpdate";
+    private static final int MY_REQUEST_CODE = 1337;
 
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
 
-    Log.d(TAG, "Initializing NetonCordovaInAppUpdate");
-  }
+        Log.d(TAG, "Initializing NetonCordovaInAppUpdate");
+    }
 
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-          if (action.equals("updateImmediate")) {
-              updateImmediate(callbackContext);
-          } else {
-              callbackContext.error("Sorry, updateFlexible not implemented yet.");
-          }
-      } else {
-          callbackContext.error("Android SDK less than 5.0");
-      }
-  }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (action.equals("updateImmediate")) {
+                updateImmediate(callbackContext);
+            } else {
+                callbackContext.error("Sorry, updateFlexible not implemented yet.");
+            }
+        } else {
+            callbackContext.error("Android SDK less than 5.0");
+        }
 
-  private void updateImmediate(CallbackContext callbackContext) {
-      Context context = this.cordova.getActivity().getApplicationContext();
+        return true;
+    }
 
-      // Creates instance of the manager.
-      AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
+    private void updateImmediate(CallbackContext callbackContext) {
+        Log.d(TAG, "updateImmediate");
+        Context context = this.cordova.getActivity().getApplicationContext();
+        Activity that = this.cordova.getActivity();
 
-      // Returns an intent object that you use to check for an update.
-      Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+        // Creates instance of the manager.
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(context);
 
-      // Checks that the platform will allow the specified type of update.
-      appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
-          if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
-                  // For a flexible update, use AppUpdateType.FLEXIBLE
-                  && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-              // Request the update.
+        // Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
 
-              appUpdateManager.startUpdateFlowForResult(
-                      // Pass the intent that is returned by 'getAppUpdateInfo()'.
-                      appUpdateInfo,
-                      // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
-                      AppUpdateType.IMMEDIATE,
-                      // The current activity making the update request.
-                      this,
-                      // Include a request code to later monitor this update request.
-                      MY_REQUEST_CODE
-              );
-          }
-      });
-  }
+        // Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            Log.d(TAG, "appUpdateInfo");
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    // For a flexible update, use AppUpdateType.FLEXIBLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                // Request the update.
+
+                try {
+                    Log.d(TAG, "appUpdateManager");
+                    appUpdateManager.startUpdateFlowForResult(
+                            // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                            appUpdateInfo,
+                            // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                            AppUpdateType.IMMEDIATE,
+                            // The current activity making the update request.
+                            that,
+                            // Include a request code to later monitor this update request.
+                            MY_REQUEST_CODE
+                    );
+                } catch (IntentSender.SendIntentException e) {
+                    Log.d(TAG, "Catch");
+                    e.printStackTrace();
+                }
+            } else {
+                Log.d(TAG, "UPDATE AVAILABILITY");
+                Log.d(TAG, "");
+            }
+        });
+    }
 
 }

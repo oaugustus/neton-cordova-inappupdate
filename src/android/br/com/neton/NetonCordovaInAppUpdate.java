5a0e9ef0eb5c86ab7bb2189d51055e9145014545
Otávio Fernandes <oaugustus@gmail.com>
2019-08-23 18:59:02 -0300

    Fix

--- src/android/br/com/neton/NetonCordovaInAppUpdate.java
+++ src/android/br/com/neton/NetonCordovaInAppUpdate.java
@@ -17,6 +17,8 @@ import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.install.model.AppUpdateType;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
 import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.Task;
+import com.google.android.play.core.appupdate.AppUpdateInfo;
 
 
 import android.util.Log;
